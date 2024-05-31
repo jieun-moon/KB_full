d6950fb4333af7ff317429e5fb6eaac76dd58f8e
Moon Ji Eun <jieun7006@naver.com>
2024-05-31 17:37:55 +0900

    first commit

---
+++ 03_Vue/chapter08/layout-slot-test/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <Layout>
+    <!-- slot을 전달해줄 때는 무조건 자식 컴포넌트의 사이에 넣어준다 -->
+    <template v-slot:header>
+      <h2>헤더 영역</h2>
+    </template>
+
+    <template v-slot:sidebar>
+      <h3>사이드</h3>
+      <h3>사이드</h3>
+      <h3>사이드</h3>
+    </template>
+
+    <template v-slot:default>
+      <h1>컨텐트 영역</h1>
+      <h1>컨텐트 영역</h1>
+      <h1>컨텐트 영역</h1>
+      <h1>컨텐트 영역</h1>
+      <h1>컨텐트 영역</h1>
+      <h1>컨텐트 영역</h1>
+    </template>
+
+    <template v-slot:footer>
+      <h2>Footer text</h2>
+    </template>
+  </Layout>
+</template>
+
+<script>
+import Layout from './components/Layout.vue';
+export default {
+  name: 'App',
+  // components 적용 안되는 건 script script 뿐임
+  components: { Layout },
+};
+</script>
---
+++ 03_Vue/chapter08/layout-slot-test/src/components/CheckBoxList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-for="item in items" :key="item.id">
+    <!-- 슬롯에 checked 값을 담아서 부모에게 보냄 -->
+    <slot name="icon" :checked="item.checked"></slot>
+    <input
+      type="checkbox"
+      :value="item.id"
+      :checked="item.checked"
+      @change="
+        $emit('check-changed', { id: item.id, checked: $event.target.checked })
+      "
+    />
+    <!-- 기본 슬롯에 checked와 label 값을 담아서 부모에게 보냄 -->
+    <slot :checked="item.checked" :label="item.label"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CheckBoxList',
+  // 부모에게서 items 받아옴
+  props: [items],
+};
+</script>
