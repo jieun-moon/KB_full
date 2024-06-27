a476224420a15579ac288db6fcf8dd9f4b302418
Moon Jieun <jieun7006@naver.com>
2024-06-28 00:08:09 +0900

    totalhap 마무리

--- 04_Java/Labs/ch04/src/L2/totalhap.java
+++ 04_Java/Labs/ch04/src/L2/totalhap.java
@@ -2,10 +2,13 @@ package L2;
 
 public class totalhap {
     public static void main(String[] args) {
-        int x;
+        int sum=0;
 
-        for(int x; x <= 100; 3x){
-            return
+        for(int i=1; i <= 100; i++){
+            if(i%3==0){
+                sum+=i;
+            }
         }
+        System.out.println("1~100까지의 정수 중에서 3의 배수의 총합: " + sum);
     }
 }
---
+++ src/totalhap.java
@@ -0,0 +1,11 @@
+package L2;
+
+public class totalhap {
+    public static void main(String[] args) {
+        int x;
+
+        for(int x; x <= 100; 3x){
+            return
+        }
+    }
+}
