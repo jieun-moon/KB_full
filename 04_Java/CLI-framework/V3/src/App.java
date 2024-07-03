be3dd42c787f4d09a6a33f26778b509611e40d73
Moon Ji Eun <jieun7006@naver.com>
2024-07-03 17:51:58 +0900

    cli v3 InitScoresCommand error fix

---
+++ 04_Java/CLI-framework/V3/src/App.java
@@ -0,0 +1,79 @@
+import java.awt.*;
+import java.util.Scanner;
+
+public class App {
+    int studentNum = 0;
+    int[] scores = null;
+
+    Scanner scanner = new Scanner(System.in);
+
+    Menu menu;
+
+    public App(){
+        //생성자에서 Menu로 객체 생성해서 초기화
+        menu = new Menu();
+    }
+    private void analize(){
+        int max = 0;
+        int sum = 0;
+        double avg = 0;
+        for (int i = 0; i < scores.length; i++) {
+            max = (max<scores[i]) ? scores[i]:max;
+            sum += scores[i];
+        }
+        avg = (double)sum/studentNum;
+        System.out.println("최고 점수: " + max);
+        System.out.println("평균 점수: " + avg);
+    }
+
+    //getStudentNum: 1번 메뉴인 학생 수 입력 기능 처리하는 메소드
+    public void getStudentNum(){
+        System.out.print("학생수> ");
+        studentNum = Integer.parseInt(scanner.nextLine());
+        scores = new int[studentNum];
+    }
+
+    public void getScores() {
+        for (int i = 0; i < scores.length; i++) {
+            System.out.print("scores[" + i + "]> ");
+            scores[i] = Integer.parseInt(scanner.nextLine());
+        }
+    }
+
+    public void printScore(){
+        for (int i = 0; i < scores.length; i++) {
+            System.out.println("scores[" + i + "]: " + scores[i]);
+        }
+    }
+
+
+
+    //executeCommand: 만들어둔 메소드들을 사용자의 입력값에 따라 호출
+    public void executeCommand(int selectNo){
+        if(selectNo==1){
+            getStudentNum();
+        } else if (selectNo == 2 ) {
+            getScores();
+        } else if (selectNo == 3 ) {
+            printScore();
+        } else if(selectNo == 4 ) {
+            analize();
+        }
+
+    }
+    public void run(){
+        while(true){
+            menu.printMenu();
+            int selectNo = menu.getSelect();
+            executeCommand(selectNo);
+            if (selectNo == 5 ) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.run();
+    }
+}
--- 04_Java/CLI-framework/V3/src/command/InitScoresCommand.java
+++ 04_Java/CLI-framework/V3/src/command/InitScoresCommand.java
@@ -1,13 +1,17 @@
-//package command;
-//
-//import domain.StudentScores;
-//
-//public class InitScoresCommand implements Command {
-//    StudentScores studentScores = StudentScores.getInstance();
-//
-//    @Override
-//    public void execute() {
-//        int studentNum = Input.getInt("학생수> ");
-//        studentScores.setStudentNum(studentNum);
-//    }
-//}
+package command;
+
+import domain.StudentScores;
+import basic.Input;
+
+public class InitScoresCommand implements Command {
+    StudentScores studentScores = StudentScores.getInstance();
+
+    @Override
+    public void execute() {
+        //Input은 package가 있어야 찾아짐
+        //getInt를 사용해서 사용자한테 학생 수를 입력받음
+        int studentNum = Input.getInt("학생수> ");
+        //studentNum과 점수 배열의 크기를 할당
+        studentScores.setStudentNum(studentNum);
+    }
+}
